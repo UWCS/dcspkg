19019bde89d0184af180248fb566d68456448dd8
Joey Harrison <joeyh021@icloud.com>
2022-09-06 21:08:35 +0100

    return optional in pkgdata handler

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -13,9 +13,10 @@ pub async fn list(mut db: Connection<PackageDB>) -> Json<Vec<Package>> {
 }
 
 #[get("/pkgdata/<name>")]
-pub async fn pkgdata(mut db: Connection<PackageDB>, name: &str) -> Json<Package> {
+pub async fn pkgdata(mut db: Connection<PackageDB>, name: &str) -> Json<Option<Package>> {
     match get_package_by_name(&mut *db, name).await {
-        Ok(x) => Json(x),
+        Ok(x) => Json(Some(x)),
+        Err(sqlx::Error::RowNotFound) => Json(None),
         Err(e) => panic!("{e:?}"), //TODO, work out how to handle failure in reponder
     }
 }
