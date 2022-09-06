68fb7d973291c0ebb31b27366510fd37af55fc85
Joey Harrison <joeyh021@icloud.com>
2022-09-06 20:24:46 +0100

    change https to http in temp config

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -1,3 +1,3 @@
 //! this should eventually be replaced with some proper config struct read from a file somwhere
 
-pub const SERVER_URL: &str = "https://127.0.0.1";
+pub const SERVER_URL: &str = "http://127.0.0.1:8000";
