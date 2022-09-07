f47c00f83dca5067cd57666b6759b5696352361d
Joey Harrison <joeyh021@icloud.com>
2022-09-07 23:40:48 +0100

    write archive func

--- packager/src/archive.rs
+++ packager/src/archive.rs
@@ -1,7 +1,15 @@
 use anyhow::Result;
+use flate2::write::GzEncoder;
+use flate2::{Compression, CrcWriter};
+use std::fs::File;
 use std::path::Path;
 
 //returns crc
 pub fn make_archive(dir_path: &Path, archive_name: &str) -> Result<u32> {
-    Ok(0)
+    let archive = File::create(archive_name)?;
+    let encoder = GzEncoder::new(archive, Compression::default());
+    let encoder = CrcWriter::new(encoder);
+    let mut tar = tar::Builder::new(encoder);
+    tar.append_dir_all(".", dir_path)?;
+    Ok(tar.into_inner()?.crc().sum())
 }
