69b2ab7d9367d7ad4193b88da983814cc82d4085
Joey Harrison <joeyh021@icloud.com>
2022-09-11 13:35:15 +0100

    tidy up install and add write to pkg dir

--- dcspkg-client/src/commands/install.rs
+++ dcspkg-client/src/commands/install.rs
@@ -6,43 +6,49 @@ use reqwest::blocking::get;
 use reqwest::{StatusCode, Url};
 use std::fs::{self, Permissions};
 use std::os::unix::fs::{symlink, PermissionsExt};
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process::Command;
 use tar::Archive;
 
 pub fn install<P: AsRef<Path>>(
-    dcspkg_dir: P,
     pkg_name: &str,
     server_url: impl reqwest::IntoUrl,
+    package_dir: P,
+    bin_dir: P,
+    registry_file: P,
 ) -> Result<()> {
     let server_url = server_url
         .into_url()
         .context("Could not parse server URL")?;
+
+    let package_dir = package_dir.as_ref();
+
     //get package data
     let pkg =
         get_pkg_data(pkg_name, &server_url).context("Could not get package data from server")?;
 
-    //create directory to unpack into
-    let install_path = dcspkg_dir.as_ref().join("packages").join(pkg.name);
-    fs::create_dir_all(&install_path).context("Could not create install directory for package")?;
+    //create the install directory
+    fs::create_dir_all(&package_dir).context("Could not create install directory for package")?;
 
+    let install_dir = package_dir.join(&pkg.name);
     //download, checksum, and decompress into PKGDIR/bin
-    download_install_file(&pkg.archive_path, pkg.crc, &server_url, &install_path)
+    download_install_file(&pkg.archive_path, pkg.crc, &server_url, &install_dir)
         .context("Could not install file")?;
 
     //run install.sh if exists
     if pkg.has_installer {
-        run_install_script(&install_path).context("Could not run install script for file")?;
+        run_install_script(&install_dir).context("Could not run install script for file")?;
     }
 
     if pkg.add_to_path {
-        let bin_path = PathBuf::from(crate::PKGDIR).join("bin");
-        let exe_path = install_path.join(pkg.executable_path.context(
-            "Package is configured to add executable to path, but does not contain an executable.",
+        let exe_path = install_dir.join(pkg.executable_path.as_ref().context(
+            "Package is configured to add executable to path, but is not configured with an executable path",
         )?);
-        create_symlink(&bin_path, &exe_path)
+        create_symlink(bin_dir.as_ref(), &exe_path)
             .context("Could not create symbolic link to package executable")?;
     }
+
+    add_to_registry(registry_file.as_ref(), pkg).context("Could not add package to registry")?;
 
     Ok(())
 }
@@ -73,16 +79,16 @@ fn get_pkg_data(pkg_name: &str, server_url: &Url) -> Result<Package> {
 }
 
 fn download_install_file(
-    pkg_name: &str,
+    pkg_url: &str,
     checksum: u32,
     server_url: &Url,
-    install_path: &Path,
+    install_dir: &Path,
 ) -> Result<()> {
     let url = server_url
-        .join(format!("{}/{}", crate::FILE_ENDPOINT, pkg_name).as_ref())
+        .join(format!("{}/{}", crate::FILE_ENDPOINT, pkg_url).as_ref())
         .context("Could not parse URL")?;
 
-    log::info!("Downloading compressed package {pkg_name}.pkg from {url}...");
+    log::info!("Downloading compressed package {pkg_url} from {url}...");
 
     let response = get(url.as_ref()).context("Request failed")?;
     log::info!("Got reponse from {url}");
@@ -115,11 +121,11 @@ fn download_install_file(
 
     //unpack archive
     archive
-        .unpack(&install_path)
+        .unpack(install_dir)
         .context("Could not unpack archive")?;
 
     log::info!("Unpacked archive");
-    log::debug!("Unpacked into {:?}", &install_path);
+    log::debug!("Unpacked into {:?}", install_dir);
 
     Ok(())
 }
@@ -154,5 +160,22 @@ fn run_install_script(path: &Path) -> Result<()> {
 
 fn create_symlink(bin_path: &Path, exe_path: &Path) -> Result<()> {
     symlink(bin_path, exe_path)?;
+    Ok(())
+}
+
+fn add_to_registry(registry_file: &Path, package: Package) -> Result<()> {
+    let file = fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(registry_file)
+        .context("Could not open registry file")?;
+
+    let mut loaded: Vec<Package> =
+        serde_json::from_reader(&file).context("Could not deserialize registry file context")?;
+
+    loaded.push(package);
+
+    serde_json::to_writer(&file, &loaded).context("Could not write registry back to file")?;
+
     Ok(())
 }
--- dcspkg-client/src/lib.rs
+++ dcspkg-client/src/lib.rs
@@ -3,7 +3,6 @@ mod commands;
 const DATA_ENDPOINT: &str = "/pkgdata";
 const FILE_ENDPOINT: &str = "/download";
 const LIST_ENDPOINT: &str = "/list";
-const PKGDIR: &str = "$HOME/.dcspkg";
 
 pub use commands::install::install;
 pub use commands::list::list;
