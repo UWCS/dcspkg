e79ad3462b8770be675030553e48b4f9d26c65da
Joey Harrison <joeyh021@icloud.com>
2022-09-11 17:48:58 +0100

    create default config if not found

--- dcspkg/src/config.rs
+++ dcspkg/src/config.rs
@@ -1,8 +1,9 @@
 //! this should eventually be replaced with some proper config struct read from a file somwhere
-use config::{Config, ConfigError, Environment, File};
+use anyhow::Context;
+use config::{Config, Environment, File};
 use home::home_dir;
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 lazy_static::lazy_static! {
     pub static ref DCSPKG_DIR: PathBuf = home_dir().expect("Could not find your home directory!").join(".dcspkg");
@@ -12,24 +13,6 @@ lazy_static::lazy_static! {
 pub struct DcspkgConfig {
     pub server: Server,
     pub registry: Registry,
-}
-
-impl DcspkgConfig {
-    pub fn get() -> Result<Self, ConfigError> {
-        Config::builder()
-            .add_source(
-                File::with_name(
-                    DCSPKG_DIR
-                        .join("config")
-                        .to_str()
-                        .expect("Could not build config file name"),
-                )
-                .required(true),
-            )
-            .add_source(Environment::with_prefix("DCSPKG"))
-            .build()
-            .and_then(|c| c.try_deserialize())
-    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -60,4 +43,36 @@ impl Default for Registry {
             bin_dir: DCSPKG_DIR.join("bin"),
         }
     }
+}
+
+impl DcspkgConfig {
+    pub fn get() -> anyhow::Result<Self> {
+        let config_file_path = DCSPKG_DIR.join("config");
+
+        if !config_file_path.exists() {
+            create_default_config_file(&config_file_path)
+                .context("Could not create config file")?;
+        }
+
+        let config = Config::builder()
+            .add_source(
+                File::with_name(
+                    DCSPKG_DIR
+                        .join("config")
+                        .to_str()
+                        .expect("Could not build config file name"),
+                )
+                .required(true),
+            )
+            .add_source(Environment::with_prefix("DCSPKG"))
+            .build()?;
+
+        config.try_deserialize().map_err(Into::into)
+    }
+}
+
+fn create_default_config_file(path: &Path) -> anyhow::Result<()> {
+    let default_contents = toml::to_string_pretty(&DcspkgConfig::default())
+        .context("Error in serializing default config struct")?;
+    std::fs::write(path, default_contents).context("Could not write to file")
 }
