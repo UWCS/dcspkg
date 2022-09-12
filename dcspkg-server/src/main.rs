3edda32ec436d2608afcd2dd814a14cc21818dbc
Joey Harrison <joeyh021@icloud.com>
2022-09-12 13:46:49 +0100

    change config slightly and make package pack an env var

--- dcspkg-server/src/main.rs
+++ dcspkg-server/src/main.rs
@@ -8,10 +8,12 @@ mod handlers;
 
 #[rocket::main]
 async fn main() -> anyhow::Result<()> {
+    let package_path = std::env::var("PACKAGE_PATH").unwrap_or_else(|_| "./packages".to_owned());
+
     let _rocket = rocket::build()
         .attach(PackageDB::init())
         .mount("/", routes![list, pkgdata])
-        .mount("/download", rocket::fs::FileServer::from("packages"))
+        .mount("/download", rocket::fs::FileServer::from(package_path))
         .launch()
         .await?;
 
