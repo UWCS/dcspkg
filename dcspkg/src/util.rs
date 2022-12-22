8e0a1dff8dc8d6e734bb029729eda070bd4c6334
Joey Harrison <joeyh021@icloud.com>
2022-12-22 13:42:45 +0000

    update package list to show full name and title

--- dcspkg/src/util.rs
+++ dcspkg/src/util.rs
@@ -5,15 +5,17 @@ pub fn print_package_list(list: &[Package], raw: bool) -> Option<()> {
     if raw {
         println!("{}", serde_json::to_string(list).unwrap());
     } else {
-        let mut table = Table::new("{:<}   {:<}").with_row(
+        let mut table = Table::new("{:<}  {:<}  {:<}").with_row(
             Row::new()
-                .with_cell("Package Name")
+                .with_cell("Game/App Name")
+                .with_cell("Package Shortnam")
                 .with_cell("Description"),
         );
         for pkg in list {
             table.add_row(
                 Row::new()
                     .with_cell(&pkg.fullname)
+                    .with_cell(&pkg.pkgname)
                     .with_cell(pkg.description.as_deref().unwrap_or("-")),
             );
         }
